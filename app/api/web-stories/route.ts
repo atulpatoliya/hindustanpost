7f3e27e28455d1fecf9fde609b8a8fd4c8d0f427
hiren <hiren02324@gmail.com>
2025-12-02 15:09:39 +0530

    Refactor web stories to fetch from API and add types

--- app/api/web-stories/route.ts
+++ app/api/web-stories/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 import getSignedUrl from "../../../lib/getSignedUrl";
 
+export const dynamic = 'force-dynamic'; 
+
 const BASE_BACKEND_URL = process.env.NEWS_BACKEND_BASE_URL;
 
 export async function GET() {
   try {
-    const baseURL = `${BASE_BACKEND_URL}/api/v1/signed-url/web-story?status=published`;
+    const baseURL = `${BASE_BACKEND_URL}/api/v1/signed-url/web-stories?status=published`;
     const signedUrl = getSignedUrl(baseURL, 60);
 
     const response = await fetch(signedUrl, { cache: "no-store" });
---
+++ types/stories.ts
@@ -0,0 +1,33 @@
+
+export interface Story {
+  _id: string;
+  title: string;
+  description: string;
+  publishedAt:string
+  author: {
+    _id: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+  } | string;
+  createdAt: string;
+  updatedAt: string;
+  status: 'active' | 'draft' | 'archived';
+  permaLink?: string;
+  slug?: string;
+  image?: string[];
+  posterImage?: string;
+  logo?: string;
+  backgroundAudio?: string;
+  visibility?: 'public' | 'private';
+  category?: string;
+  tag?: string;
+  seo?: {
+    metaTitle?: string;
+    metaDescription?: string;
+    metaKeywords?: string[];
+    slug?: string;
+    canonicalUrl?: string;
+    robots?: string;
+  };
+}
